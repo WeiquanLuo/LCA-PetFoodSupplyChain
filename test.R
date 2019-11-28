504ccb3dc00c44fa59370554ee1f1f15f5cc6799
WeiquanLuo <weiquan.luo1221@gmail.com>
2019-11-28 12:53:15 -0600

    modify

--- test.R
+++ test.R
@@ -6,21 +6,29 @@ library(dplyr)
 library(purrr)
 library(stringr)
 library(plyr)
+
+
 #multiple csv input
 temp = list.files(path = "data", pattern="*xls$")
-tempname = sapply(strsplit(temp, split='.', fixed=TRUE), function(x) (x[1]))
+tempname = sapply(strsplit(temp, split='.', fixed=TRUE), function(x) (x[1] %>% str_replace("311111_1M_","") %>% str_replace("_all","")))
 for (i in 1:length(temp)){
   assign(tempname[i], gdata::read.xls(file.path("data", temp[i])) %>% 
            select(-"X.."))
 }
 
-#heatmap(as.matrix.data.frame(data))
+# obtain colname as individual vector
 cols <- readxl::read_excel("data/Columns name.xlsx", col_names=FALSE)
-
-
 for (i in 1: nrow(cols)){
   col <-cols[i,]
   col <- col %>% flatten_chr() %>% na.omit()
   col1 <- str_replace_all(col[1], "[[:space:]]",""); print(col1)
   assign(col1, col[-1])
 }
+
+
+
+
+
+
+
+

76de87b41cdaca1d06c57fc96a60f4fb74b7fe23
WeiquanLuo <weiquan.luo1221@gmail.com>
2019-12-02 02:04:20 -0600

    model selection and linear model result

--- code_regsubsets.R
+++ code_regsubsets.R
@@ -23,7 +23,7 @@ print.regsub <- function(l, sort='BIC', best=NULL) {
   
   aic <- l$bic - log(l$obj$nn)*nvar + 2*nvar
   
-  temp <- data.frame(model=var, nvar=nvar, Rsq=l$rsq, AdjRsq=l$adjr2, 
+  temp <- data.frame(var=var, nvar=nvar, Rsq=l$rsq, AdjRsq=l$adjr2, 
                      Cp=l$cp, AIC = aic, BIC=l$bic)
   o <- order(temp[,sort])
   if (!is.null(best)) {
@@ -43,23 +43,23 @@ dat <- dat %>% filter(X != 17)
 CPA <- dat %>% select(CO.t, NH3.t, NOx.t, PM10.t, PM2.5.t, SO2.t, VOC.t)
 GHG <- dat %>% select(Total.t.CO2e, CO2.Fossil.t.CO2e, CO2.Process.t.CO2e, CH4.t.CO2e, HFC.PFCs.t.CO2e)
 TOX <- dat %>% select(Fugitive.kg, Stack.kg, Total.Air.kg, Surface.water.kg, U.ground.Water.kg, Land.kg, Offiste.kg, POTW.Metal.kg)
-model_list <- tibble(target = c(colnames(CPA),colnames(GHG),colnames(TOX))); model_list
+target_list <- tibble(target = c(colnames(CPA),colnames(GHG),colnames(TOX))); target_list
 
 # single example
-lm_y <- regsubsets(NH3.t ~ Coal.TJ + NatGase.TJ + Petrol.TJ + Bio.Waste.TJ + NonFossElec.TJ + Water.Withdrawals.Kgal, data = dat, method = "exhaustive", nbest = 5); lm.CPA_CO.t %>% summary()
+lm_y <- regsubsets(NH3.t ~ Sector + Coal.TJ + NatGase.TJ + Petrol.TJ + Bio.Waste.TJ + NonFossElec.TJ + Water.Withdrawals.Kgal, data = dat, method = "exhaustive", nbest = 5); lm.CPA_CO.t %>% summary()
 print.regsub(lm_y %>% summary, sort='AIC', best=1)
 
 # write a function can iterate regsubset()
 # test: target_nm = "CO.t"
 regsubsets_map <- function(target_nm, dat){
   # Input columns
-  X <- dat %>% select(Coal.TJ, NatGase.TJ, Petrol.TJ, Bio.Waste.TJ, NonFossElec.TJ, Water.Withdrawals.Kgal)
+  X <- dat %>% select(Sector, Coal.TJ, NatGase.TJ, Petrol.TJ, Bio.Waste.TJ, NonFossElec.TJ, Water.Withdrawals.Kgal)
   # targe columns
   y <- dat %>% select(target_nm)
   lm_y <- regsubsets(as.matrix(X), y[,1], method = "exhaustive", nbest = 1)
   }
 
-model_list <- model_list %>% 
+regsubsets_list <- target_list %>% 
   mutate(top_model = target %>% 
            map(function(target_nm) regsubsets_map(target_nm = target_nm,
                                                   dat= dat))) %>% 
@@ -67,6 +67,36 @@ model_list <- model_list %>%
            map(function(top_model) print.regsub(l = top_model %>% summary, 
                                             sort='AIC', 
                                             best=1))) %>% 
-  tidyr::unnest(best_model); model_list
+  tidyr::unnest(best_model) %>% 
+  mutate(var = var %>% as.character()); regsubsets_list
 
-hist(model_list$Rsq)
+hist(regsubsets_list$Rsq)
+
+fit_model <- function(data, dat){
+  f <- paste(data$target, data$var %>% stringr::str_replace_all(" ", " + "), sep = " ~ ")
+  lm_y <- lm(f, data = dat)
+  return(lm_y) 
+}
+
+
+lm_list <- regsubsets_list %>% select(c(target, var)) %>% 
+  tibble::rowid_to_column("index") %>% 
+  tidyr::nest(data = c(target, var)) %>% 
+  mutate(lm = data %>% map(function(data) fit_model(data = data, 
+                                                              dat = dat))) %>% 
+  tidyr::unnest(data) %>% 
+  mutate(anv = lm %>% map(anova)) %>% 
+  mutate(statisics = lm %>% purrr::map(.f = function(m) broom::glance(m))) %>% 
+  tidyr::unnest(statisics); lm_list
+
+
+#
+regsubsets_list # result of model selection
+lm_list # result of selected linear model
+
+
+
+
+
+
+
